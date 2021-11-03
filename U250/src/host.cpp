a74c1530b2d1d9c0a13a26558631950735840ce3
linghao.song <linghao.song@duke.edu>
2021-11-02 19:59:33 -0700

    Update host.cpp

--- U250/src/host.cpp
+++ U250/src/host.cpp
@@ -1,3 +1,33 @@
+/**********
+ Copyright (c) 2018, Xilinx, Inc.
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
+ 
+ 1. Redistributions of source code must retain the above copyright notice,
+ this list of conditions and the following disclaimer.
+ 
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+ 
+ 3. Neither the name of the copyright holder nor the names of its contributors
+ may be used to endorse or promote products derived from this software
+ without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **********/
+
+
 #include "xcl2.hpp"
 #include <cmath>
 #include <algorithm>
@@ -161,7 +191,7 @@ int main(int argc, char **argv) {
     
     vector<vector<unsigned long, aligned_allocator<unsigned long> > > sparse_A_fpga_vec(NUM_CH_SPARSE);
     int sparse_A_fpga_column_size = 8 * edge_list_ptr[edge_list_ptr.size()-1] * 4 / 4;
-    int sparse_A_fpga_chunk_size = ((sparse_A_fpga_column_size + 511)/512) * 512;
+    //int sparse_A_fpga_chunk_size = ((sparse_A_fpga_column_size + 511)/512) * 512;
     
     edge_list_64bit(edge_list_pes,
                     edge_list_ptr,
@@ -384,7 +414,6 @@ int main(int argc, char **argv) {
     
     OCL_CHECK(err, err = krnl_sextans.setArg(parameter_pos++, M));
     OCL_CHECK(err, err = krnl_sextans.setArg(parameter_pos++, K));
-    int N_parameter_pos = parameter_pos;
     int para_N = (rp_time << 16) | N;
     OCL_CHECK(err, err = krnl_sextans.setArg(parameter_pos++, para_N));
     
